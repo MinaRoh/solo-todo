bc04d6965d0d4d78746de17d009c88485ea07bba
MinaRoh <00minha@gmail.com>
2022-10-18 14:23:37 +0900

    Feat: add delete todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,21 @@
-
+import { fetchDelete } from '../util/api';
 
 const TodoList = ({ todos }) => {
 
-  // const handleDeleteClick = (id) => {
-  //   fetchDelete('http://localhost:3001/todos/', id);
-  // }
+  const handleDeleteClick = (id) => {
+    fetchDelete('http://localhost:3001/todos/', id);
+  }
 
   return (
     <div className="todo-list">
+
       {!todos ? <p>todos가 null입니다</p> :
         todos.map(todo => (
           <div className="todolist" key={todo.id} >
             {todo.isDone ? <span>☑︎</span> : <span>◻︎</span>}
             <span> {todo.id} </span>
             <span>{todo.text} </span>
+            <button onClick={() => handleDeleteClick(todo.id)}> ☒</button>
           </div>
         ))}
 
